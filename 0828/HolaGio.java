a54d5864ed89f5945dcdadba9c4f20c3fd50e163
GioMonarch <51375106+GioMonarch@users.noreply.github.com>
2021-09-18 07:49:02 -0500

    SMP tarea

--- 0828/HolaGio.java
+++ 0828/HolaGio.java
@@ -1,5 +1,5 @@
 public class HolaGio {
-    public static void main(String []args){
-        System.out.println("Hola Mundo!");
+    public static void main(Strings []args){
+        System.out.println("Hola Mundo!")
     }
 }
