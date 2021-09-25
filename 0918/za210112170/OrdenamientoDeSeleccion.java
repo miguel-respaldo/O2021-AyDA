dc11168fd16371685552b2d306dacac2d07ced40
yazmin.salinas <yazmin.salinas@ureblock.com.mx>
2021-09-25 07:30:44 -0500

    25-09-2021

--- 0918/za210112170/OrdenamientoDeSeleccion.java
+++ 0918/za210112170/OrdenamientoDeSeleccion.java
@@ -2,44 +2,34 @@
 import java.util.Scanner;
 public class OrdenamientoDeSeleccion {
     public static void main(String []args){
-        System.out.println("Ordenamiento de seleccion");
-
-        Scanner teclado= new Scanner(System.in);
-
-        System.out.print("Ingrese dimension del arreglo: ");
-        int tam=teclado.nextInt();
-        int arreglo[]= new int [tam];
-
-        //Ingresando Valores al arreglo
-        for(int i=0 ; i<arreglo.length; i++){
-            System.out.print("A["+i+"]= ");
-            arreglo[i]=teclado.nextInt();
+        // El arreglo
+        int[] arreglo = {30, 28, 11, 96, -5, 21, 18, 12, 22, 30, 97, -1, -40, -500};
+        System.out.println("Imprimiendo arreglo antes de ordenar...");
+        for (int i : arreglo) {
+            System.out.printf("%d, ", i);
         }
+        // Ordenamos. Recuerda: en Java los arreglos se pasan por referencia así que no necesitamos obtener
+        // el resultado, ya que dentro de la función se modifica al arreglo
+        // Más información: https://parzibyte.me/blog/2020/04/04/java-parametros-referencia-valor/
+        ordenarPorSeleccion(arreglo);
 
-        Burbuja(arreglo);
-
-        //Mostrando el arreglo Ordenado
-        System.out.println("===== ARREGLO ORDENADO ====");
-        for(int i =0; i<arreglo.length;i++){
-            System.out.print("\t"+arreglo[i]);
+        System.out.println("\nImprimiendo arreglo después de ordenar...");
+        for (int i : arreglo) {
+            System.out.printf("%d, ", i);
         }
-
-
     }
 
-    //Metodo de ordenamiento burbuja
-    static void Burbuja(int arreglo[]){
-        int i,j,aux;
-        for(i=0;i<arreglo.length;i++){
-            for(j=i+1;j<arreglo.length;j++){
-                if(arreglo[i]>arreglo[j]){
-                    aux=arreglo[i];
-                    arreglo[i]=arreglo[j];
-                    arreglo[j]=aux;
+    public static void ordenarPorSeleccion(int[] arreglo) {
+        for (int i = 0; i < arreglo.length - 1; i++) {
+            for (int j = i + 1; j < arreglo.length; j++) {
+                if (arreglo[i] > arreglo[j]) {
+                    // ...intercambiarlos, es decir, mover el actual a la derecha y el de la derecha al actual
+                    int temporal = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = temporal;
                 }
             }
         }
-
     }
 
 }
