444c59755ad66f8365e750e3a1dbcab77ccc5b29
yazmin.salinas <yazmin.salinas@ureblock.com.mx>
2021-10-18 16:15:04 -0500

    Merge remote-tracking branch 'origin/main' into main

--- 1023/za210112170/BinarySearch.java
+++ 1023/za210112170/BinarySearch.java
@@ -1,48 +1,30 @@
-import java.util.Scanner;
+public class BinarySearch {
 
-class BinarySearch {
-
-    Scanner in = new Scanner(System.in);
-    int[] nums = new int[5];
-
-
-        int binarySearch(int arr[], int lo, int hi, int x) {
-            if (hi >= lo && lo < arr.length - 1) {
-                int mid = lo + (hi - lo) / 2;
-                if (arr[mid] == x)
-                    return mid;
-                if (arr[mid] > x)
-                    return binarySearch(arr, lo, mid - 1, x);
-                return binarySearch(arr, mid + 1, hi, x);
+    public static int binarySearch(int[] srcArray, int des)
+    {
+        int low = 0;
+        int high = srcArray.length-1;
+        while(low <= high)
+        {
+            int middle = (low + high)/2;
+            if(des == srcArray[middle])
+            {
+                return middle;
             }
-            return -1;
-        }
-        public static void main(String args[]) {
-            BinarySearch ob = new BinarySearch();
-            Scanner in = new Scanner(System.in);
-            int[] nums = new int[5];
-
-            int arr[] = { 1, 2, 3, 4, 5 };
-            int n = arr.length;
-            int x = 2;
-
-            for (int i = 0; i < 5; i++) {
-                System.out.println("Ingrese el número" + (i + 1) + "Número:");
-                nums[i] = in.nextInt();
+            else if(des <srcArray[middle])
+            {
+                high = middle - 1;
             }
-            System.out.println("Antes de ordenar:");
-            for (int num : nums) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-
-
-            int position = ob.binarySearch(arr, 0, n - 1, x);
-            if (position == -1)
-                System.out.println("Element not found !!!");
             else
-                System.out.println("Element found at index: " + position);
+            {
+                low = middle + 1;
+            }
         }
+        return -1;
     }
-
-
+    public static void main(String[] args)
+    {
+        int[] src = new int[] {1, 3, 5, 7, 8, 9};
+        System.out.println(binarySearch(src, 3));
+    }
+}
