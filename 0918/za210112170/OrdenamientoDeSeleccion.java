6d5059494c91613e7511de9ad2e920a03a88ce2c
yazmin.salinas <yazmin.salinas@ureblock.com.mx>
2021-10-18 11:47:21 -0500

    25-09-2021

--- 0918/za210112170/OrdenamientoDeSeleccion.java
+++ 0918/za210112170/OrdenamientoDeSeleccion.java
@@ -8,10 +8,7 @@ public class OrdenamientoDeSeleccion {
         for (int i : arreglo) {
             System.out.printf("%d, ", i);
         }
-        // Ordenamos. Recuerda: en Java los arreglos se pasan por referencia así que no necesitamos obtener
-        // el resultado, ya que dentro de la función se modifica al arreglo
-        // Más información: https://parzibyte.me/blog/2020/04/04/java-parametros-referencia-valor/
-        ordenarPorSeleccion(arreglo);
+         ordenarPorSeleccion(arreglo);
 
         System.out.println("\nImprimiendo arreglo después de ordenar...");
         for (int i : arreglo) {
@@ -23,7 +20,6 @@ public class OrdenamientoDeSeleccion {
         for (int i = 0; i < arreglo.length - 1; i++) {
             for (int j = i + 1; j < arreglo.length; j++) {
                 if (arreglo[i] > arreglo[j]) {
-                    // ...intercambiarlos, es decir, mover el actual a la derecha y el de la derecha al actual
                     int temporal = arreglo[i];
                     arreglo[i] = arreglo[j];
                     arreglo[j] = temporal;
