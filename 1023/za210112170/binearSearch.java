2e3382502cf4528245ea0137b8ec8ab1def2d127
yazmin.salinas <yazmin.salinas@ureblock.com.mx>
2021-10-18 16:15:04 -0500

    Merge remote-tracking branch 'origin/main' into main

---
+++ 1023/za210112170/binearSearch.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class binearSearch {
+
+    public static void main(String[] args){
+        Scanner sr = new Scanner(System.in);
+
+        int f = sr.nextInt ();
+        int n = sr.nextInt ();
+        int attr [] = new int [n];
+        for (int i=0;i<n;i++){
+            attr[i]=sr.nextInt();
+        }
+        int index = searchmy(attr,f,n);
+        if (index==-1){
+            System.out.println ("No encontrado");
+        }else{
+            System.out.println (f + "encontró que el índice es:" + index);
+        }
+    }
+    static int  searchmy(int att[],int m ,int n){
+        int low = 0;
+        int high=att.length-1;
+
+        return searchmy(att,m,n,low,high);
+    }
+
+    static int  searchmy(int att[],int m ,int n,int low,int high){
+        if(low >high){
+            return -1;
+        }
+        int mid = (low+high)/2;
+        if (m==att[mid]){
+            return mid;
+        }else if (m<att[mid]){
+            return searchmy(att,m,n,low,mid-1);
+        }else {
+            return searchmy(att,m,n,mid+1,high);
+        }
+    }
+}
