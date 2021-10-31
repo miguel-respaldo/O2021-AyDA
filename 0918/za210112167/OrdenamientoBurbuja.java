d5b256415df9ac4cca5c7c88f4ba8c6037aeb9ce
Cristopher Pelayo <Cristopher Pelayo>
2021-10-31 07:22:00 -0600

    comparacion

--- 0918/za210112167/OrdenamientoBurbuja.java
+++ 0918/za210112167/OrdenamientoBurbuja.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
-class OrdenamientoBurbujag {
+class OrdenamientoBurbuja {
 
     public static void main(String[] args) {
         System.out.println("Ordenamiento Burbuja");
 
-        int numeros[] = {1, 9, 23, 4, 55, 100, 1, 1, 23};
+        int[] numeros = {1, 9, 23, 4, 55, 100, 1, 1, 23};
         System.out.println("Antes del método de la burbuja: " + Arrays.toString(numeros));
         burbuja(numeros);
         System.out.println("Después del método de la burbuja: " + Arrays.toString(numeros));
@@ -14,7 +14,7 @@ class OrdenamientoBurbujag {
         System.out.println("Después del método de la burbuja: " + Arrays.toString(nombres));
     }
 
-    private static void burbuja(int[] arreglo) {
+    public static void burbuja(int[] arreglo) {
         for (int x = 0; x < arreglo.length; x++) {
             // Aquí "y" se detiene antes de llegar
             // a length - 1 porque dentro del for, accedemos
