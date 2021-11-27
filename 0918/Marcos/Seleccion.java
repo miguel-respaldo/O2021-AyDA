f544d09a09643befe8db4c5c5106c0a3ded3389f
Marcos Vazquez <marcos.a.vazquez.b@gmail.com>
2021-11-26 21:17:13 -0600

    Metodo Seleccion tarea

---
+++ 0918/Marcos/Seleccion.java
@@ -0,0 +1,26 @@
+public class Seleccion {
+    public static int[] ordena(int[] arreglo){
+        int i,j;
+        int temp;
+        for(i=0;i<arreglo.length-1;i++){
+            for(j=0;j<arreglo.length-1-i;j++){
+                if (arreglo[j]>arreglo[j+1]){
+                    temp=arreglo[j+1];
+                    arreglo[j+1]=arreglo[j];
+                    arreglo[j]=temp;
+                }
+            }
+        }
+        return arreglo;
+    }
+
+    public static void main(String[] args) {
+        /* code */
+        int i;
+        int arreglo[]={15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+        int arOrdenado[] = ordena(arreglo);
+        for(i=0;i<arOrdenado.length;i++)
+            System.out.println(arOrdenado[i]);
+
+    }
+}
