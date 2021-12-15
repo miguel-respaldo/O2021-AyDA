f508b15fa215b16f8f87ff35b62a0411e76a9313
Hommar-alvarez <95763877+Hommar-alvarez@users.noreply.github.com>
2021-12-15 17:00:10 -0600

    Add files via upload

---
+++ 0918/burbujaByHom.java
@@ -0,0 +1,51 @@
+// metodo de burbuja
+//by hommar
+
+import java.util.Arrays;
+class OrdenamientoBurbuja {
+
+    public static void main(String[] args) {
+        System.out.println("Ordenamiento Burbuja");
+
+        int[] numeros = {1, 9, 23, 4, 55, 100, 1, 1, 23};
+        System.out.println("Antes del método de burbuja: " + Arrays.toString(numeros));
+        burbuja(numeros);
+        System.out.println("Después del método de burbuja: " + Arrays.toString(numeros));
+        String[] nombres = {"Leon", "Chris", "Jill", "Wesker", "Ada"};
+        System.out.println("Antes del método de burbuja: " + Arrays.toString(nombres));
+        burbuja(nombres);
+        System.out.println("Después del método de burbuja: " + Arrays.toString(nombres));
+    }
+
+    public static void burbuja(int[] arreglo) {
+        for (int x = 0; x < arreglo.length; x++) {
+            // Aquí "y" se detiene antes de llegar
+            // a length - 1 porque dentro del for, accedemos
+            // al siguiente elemento con el índice actual + 1
+            for (int y = 0; y < arreglo.length - 1; y++) {
+                int elementoActual = arreglo[y], elementoSiguiente = arreglo[y + 1];
+                if (elementoActual > elementoSiguiente) {
+                    // Intercambiar
+                    arreglo[y] = elementoSiguiente;
+                    arreglo[y + 1] = elementoActual;
+                }
+            }
+        }
+    }
+
+    private static void burbuja(String[] arreglo) {
+        for (int x = 0; x < arreglo.length; x++) {
+            // Aquí "y" se detiene antes de llegar
+            // a length - 1 porque dentro del for, accedemos
+            // al siguiente elemento con el índice actual + 1
+            for (int y = 0; y < arreglo.length - 1; y++) {
+                String elementoActual = arreglo[y], elementoSiguiente = arreglo[y + 1];
+                if (elementoActual.compareTo(elementoSiguiente) > 0) {
+                    // Intercambiar
+                    arreglo[y] = elementoSiguiente;
+                    arreglo[y + 1] = elementoActual;
+                }
+            }
+        }
+    }
+}
