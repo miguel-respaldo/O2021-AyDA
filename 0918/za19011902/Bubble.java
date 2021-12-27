a7bd38b98836eee6122f5d497a4db13fa56059d9
Ramon Mata <mataramon@users.noreply.github.com>
2021-12-27 12:38:51 -0600

    Update and rename bubble.java to Bubble.java

--- 0918/za19011902/Bubble.java
+++ 0918/za19011902/Bubble.java
@@ -1,17 +1,17 @@
 import java.util.Arrays;
-public class bubble {
+public class Bubble {
 
     public static void main(String[] args) {
         System.out.println("Bubble ordering...");
 
         int numbers[] = {15, 8, 33, 40, 5, 10, 17, 7, 23};
         System.out.println("Init: " + Arrays.toString(numbers));
-        bubbleOrder(numbers);
+        bubbleSort(numbers);
         System.out.println("After: " + Arrays.toString(numbers));
 
     }
 	
-    private static void bubbleOrder(int[] numbers) {
+    private static void bubbleSort(int[] numbers) {
         for (int x = 0; x < numbers.length; x++) {
             for (int y = 0; y < numbers.length - 1; y++) {
                 int a = numbers[y], b = numbers[y + 1];
@@ -22,4 +22,3 @@ public class bubble {
             }
         }
     }
-}
