b340c5bc1b5dd6ddbbef47e6945a65a2ad4fbc40
Ramon Mata <mataramon@users.noreply.github.com>
2021-12-27 12:24:13 -0600

    Add files via upload

---
+++ 0918/za19011902/bubble.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+public class bubble {
+
+    public static void main(String[] args) {
+        System.out.println("Bubble ordering...");
+
+        int numbers[] = {15, 8, 33, 40, 5, 10, 17, 7, 23};
+        System.out.println("Init: " + Arrays.toString(numbers));
+        bubbleOrder(numbers);
+        System.out.println("After: " + Arrays.toString(numbers));
+
+    }
+	
+    private static void bubbleOrder(int[] numbers) {
+        for (int x = 0; x < numbers.length; x++) {
+            for (int y = 0; y < numbers.length - 1; y++) {
+                int a = numbers[y], b = numbers[y + 1];
+                if (a > b) {
+                    numbers[y] = b;
+                    numbers[y + 1] = a;
+                }
+            }
+        }
+    }
+}
