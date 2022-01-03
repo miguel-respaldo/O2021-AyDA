e2e211279c46ac7bd5ef5a784058aa730bce9f7d
Ramon Mata <mataramon@users.noreply.github.com>
2022-01-02 22:00:50 -0600

    Sequential Search

---
+++ 1023/za19011902/sequentialSearch.java
@@ -0,0 +1,27 @@
+public class sequentialSearch {
+
+
+    public static void main(String args[]) {
+
+        int arreglo[] = {1,3,5,2,4};
+        int dato;
+        boolean bandera = false;
+
+        dato = Integer.parseInt(JOptionPane.showInputDialog("Digite el numero a buscar"));
+
+        int i=0;
+
+        while(i<5 && bandera == false){
+            if(arreglo[i]== dato){
+                bandera = true;
+            }
+            i++;
+        }
+        if(bandera==false){
+            JOptionPane.showMessageDialog(null,"El Numero no se encuentra en el arreglo" + dato);
+        }
+        else{
+            JOptionPane.showMessageDialog(null,"El numero esta en el arreglo en posicion" + (i-1));
+        }
+    }
+}
